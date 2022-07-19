6dcd836ad1ee9e578d08a48e9a9ddf728665a57c
Lucas Lamonier <lucasrlamonier@gmail.com>
2022-07-19 09:36:03 -0300

    Finish fixing killer power icons

--- data/importdevdata.js
+++ data/importdevdata.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const Item = require("./../models/itemModel");
 const KillerAddon = require("./../models/killerAddonModel");
@@ -9,63 +10,44 @@ const SurvivorPerk = require("./../models/survivorPerkModel");
 const KillerPerk = require("./../models/killerPerkModel");
 const Killer = require("./../models/killerModel");
 
+dotenv.config({ path: "./config.env" });
 const DB = process.env.DATABASE;
 
 mongoose.connect(DB, {}).then(() => console.log("DB online!"));
 
-// const itemAddons = JSON.parse(
-// 	fs.readFileSync(`${__dirname}/itemAddons.json`, "utf-8")
-// );
-// const items = JSON.parse(fs.readFileSync(`${__dirname}/items.json`, "utf-8"));
-// const killerAddons = JSON.parse(
-// 	fs.readFileSync(`${__dirname}/killerAddons.json`, "utf-8")
-// );
-// const killerPerks = JSON.parse(
-// 	fs.readFileSync(`${__dirname}/killerPerks.json`, "utf-8")
-// );
-// const powers = JSON.parse(
-// 	fs.readFileSync(`${__dirname}/killerPowers.json`, "utf-8")
-// );
-// const killers = JSON.parse(
-// 	fs.readFileSync(`${__dirname}/killers.json`, "utf-8")
-// );
-// const survivorPerks = JSON.parse(
-// 	fs.readFileSync(`${__dirname}/survivorPerks.json`, "utf-8")
-// );
-// const survivors = JSON.parse(
-// 	fs.readFileSync(`${__dirname}/survivors.json`, "utf-8")
-// );
-
-// Patch 6.0.0
+const itemAddons = JSON.parse(
+	fs.readFileSync(`${__dirname}/itemAddons.json`, "utf-8")
+);
+const items = JSON.parse(fs.readFileSync(`${__dirname}/items.json`, "utf-8"));
 const killerAddons = JSON.parse(
-	fs.readFileSync(`${__dirname}/6.0.0/newKillerAddons.json`, "utf-8")
+	fs.readFileSync(`${__dirname}/killerAddons.json`, "utf-8")
 );
 const killerPerks = JSON.parse(
-	fs.readFileSync(`${__dirname}/6.0.0/newKillerPerks.json`, "utf-8")
+	fs.readFileSync(`${__dirname}/killerPerks.json`, "utf-8")
 );
 const powers = JSON.parse(
-	fs.readFileSync(`${__dirname}/6.0.0/newKillerPowers.json`, "utf-8")
+	fs.readFileSync(`${__dirname}/killerPowers.json`, "utf-8")
 );
 const killers = JSON.parse(
-	fs.readFileSync(`${__dirname}/6.0.0/newKillers.json`, "utf-8")
+	fs.readFileSync(`${__dirname}/killers.json`, "utf-8")
 );
 const survivorPerks = JSON.parse(
-	fs.readFileSync(`${__dirname}/6.0.0/newSurvivorPerks.json`, "utf-8")
+	fs.readFileSync(`${__dirname}/survivorPerks.json`, "utf-8")
 );
 const survivors = JSON.parse(
-	fs.readFileSync(`${__dirname}/6.0.0/newSurvivors.json`, "utf-8")
+	fs.readFileSync(`${__dirname}/survivors.json`, "utf-8")
 );
 
 const importData = async () => {
 	try {
 		// await ItemAddon.create(itemAddons);
 		// await Item.create(items);
-		await KillerAddon.create(killerAddons);
-		await KillerPerk.create(killerPerks);
-		await KillerPower.create(powers);
-		await Killer.create(killers);
-		await SurvivorPerk.create(survivorPerks);
-		await Survivor.create(survivors);
+		// await KillerAddon.create(killerAddons);
+		// await KillerPerk.create(killerPerks);
+		// await KillerPower.create(powers);
+		// await Killer.create(killers);
+		// await SurvivorPerk.create(survivorPerks);
+		// await Survivor.create(survivors);
 		console.log("Data imported!");
 		process.exit();
 	} catch (err) {
