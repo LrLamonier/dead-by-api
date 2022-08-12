17a00cf0a9438a20156b2c636a6fe611aa4945ad
Lucas Lamonier <lucasrlamonier@gmail.com>
2022-08-12 09:08:54 -0300

    Correct a typo on server.js

--- server.js
+++ server.js
@@ -3,25 +3,25 @@ const dotenv = require("dotenv");
 const app = require("./app");
 
 process.on("uncaughtException", (err) => {
-	server.close(() => {
-		process.exit(1);
-	});
+  server.close(() => {
+    process.exit(1);
+  });
 });
 
 dotenv.config({ path: "./config.env" });
 
 mongoose
-	.connect(process.env.DATABASE, {})
-	.then(() => console.log("Database online!"));
+  .connect(process.env.DATABASE, {})
+  .then(() => console.log("Database online!"));
 
 const port = process.env.PORT || 80;
 
 const server = app.listen(port, () => {
-	console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
 
-process.on("unhandledRefection", (err) => {
-	server.close(() => {
-		process.exit(1);
-	});
+process.on("unhandledRejection", (err) => {
+  server.close(() => {
+    process.exit(1);
+  });
 });
